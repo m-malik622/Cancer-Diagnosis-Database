f16fe5bcfbe18a310fa938b6b3f8972972d4147e
vrenee0 <101755623+vrenee0@users.noreply.github.com>
2025-11-26 11:04:38 -0600

    endpoints
    views and urls for doctor and patients

--- backend/cancer_diagnosis_database/database/urls.py
+++ backend/cancer_diagnosis_database/database/urls.py
@@ -5,6 +5,19 @@ urlpatterns = [
     path("", views.index, name="index"),
 ]
 
+#Patient
 urlpatterns = [
-    path("", views.Create_Patient, name="Create_Patient"),
+    path("create_catient", views.create_patient),
+    path("get_patients/", views.get_patients),
+    path("get_patients/<int:id>/", views.get_patient),
+    path("update_patient/<int:id>/", views.update_patient),
+    path("delete_patient/<int:id>/", views.delete_patient),      
+]
+#Doctor
+urlpatterns = [
+    path("create_doctor/", views.create_doctor),
+    path("get_doctors/", views.get_doctors),
+    path("get_doctor/<int:id>/", views.get_doctor),
+    path("update_doctor/<int:id>/", views.update_doctor),
+    path("delete_doctor/<int:id>/", views.delete_doctor),
 ]
