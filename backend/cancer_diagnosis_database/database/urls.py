15ac8132bd1d565eddae3e698174fa88ccfe3f97
Malik <m.malik62205@gmail.com>
2025-11-28 00:17:14 -0600

    refactoed urls. Now all shojld work

--- backend/cancer_diagnosis_database/database/urls.py
+++ backend/cancer_diagnosis_database/database/urls.py
@@ -59,20 +59,3 @@ urlpatterns = [
     path("create_diagnosis", views.create_diagnosis),
     path("remove_diagnosis/<int:diagnosis_id>", views.remove_diagnosis),
 ]
-
-#Patient
-urlpatterns = [
-    path("create_catient", views.create_patient),
-    path("get_patients/", views.get_patients),
-    path("get_patients/<int:id>/", views.get_patient),
-    path("update_patient/<int:id>/", views.update_patient),
-    path("delete_patient/<int:id>/", views.delete_patient),      
-]
-#Doctor
-urlpatterns = [
-    path("create_doctor/", views.create_doctor),
-    path("get_doctors/", views.get_doctors),
-    path("get_doctor/<int:id>/", views.get_doctor),
-    path("update_doctor/<int:id>/", views.update_doctor),
-    path("delete_doctor/<int:id>/", views.delete_doctor),
-]
